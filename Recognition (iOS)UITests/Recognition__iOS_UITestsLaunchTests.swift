123376e47e524ddd990aab8b2cc799721d015258
ahm3d19 <uolf2019@gmail.com>
2022-12-19 18:07:54 +0500

    Initial Commit

---
+++ Recognition (iOS)UITests/Recognition__iOS_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Recognition__iOS_UITestsLaunchTests.swift
+//  Recognition (iOS)UITests
+//
+//  Created by AHM3D_MAC on 19/12/2022.
+//
+
+import XCTest
+
+final class Recognition__iOS_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
